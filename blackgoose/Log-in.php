dd87d9ccf6a7de28f70eef2eec0dc1ea6ea8bf8f
Michael Anthony Alfonso <77504929+malfonso94848@users.noreply.github.com>
2023-04-30 01:44:57 -0400

    Temporary Delete.

--- blackgoose/Log-in.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if(isset($_COOKIE["signedup"]) ) {
-        echo("You were logged in using cookies.");
-    } else {
-        echo("We appologize but we could not find the cookies stored on your computer.")
-    }
